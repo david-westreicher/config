9bfb415f34bba0298e026ee23feeac949e0171a1
david-westreicher <david.westreicher@gmail.com>
2015-04-25 21:48:47 +0200

    fix install script

--- installconfigs.sh
+++ installconfigs.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
-echo "Trying to install Vundle..."
-if hash git 2>/dev/null; then
-    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-    if hash vim 2>/dev/null; then
-        # Fix bashrc
-        cat addtobashrc >> ~/.bashrc
-        source ~/.bashrc
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cat addtobashrc >> ~/.bashrc
+source ~/.bashrc
 
-        DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-        ln -s "$DIR/vimrc" ~/.vimrc
-        vim +PluginInstall
-    else
-        echo "You need to install VIM!"
-    fi
-else
-    echo "You need to install GIT!"
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+if [ -f ~/.vimrc ];
+then
+    echo "deleting"
+    rm ~/.vimrc
 fi
+ln -s $DIR/vimrc ~/.vimrc
+
+if [ -f ~/.i3/config ];
+then
+    rm ~/.i3/config
+fi
+mkdir ~/.i3 -p
+ln -s $DIR/i3config ~/.i3/config
+
+if [ -f ~/.config/lilyterm/default.conf ];
+then
+    rm ~/.config/lilyterm/default.conf 
+fi
+mkdir ~/.config/lilyterm -p
+ln -s $DIR/lilyterm.conf ~/.config/lilyterm/default.conf
+
+vim +PluginInstall
