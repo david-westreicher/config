44b3a52208a0b8b84745bf78f5958cf5982ecad2
David Westreicher <david.westreicher@gmail.com>
2016-12-23 19:55:09 +0100

    fix vundle install

--- install.sh
+++ install.sh
@@ -5,9 +5,10 @@ mkdir -p ~/.i3
 
 git clone https://github.com/david-westreicher/config ~/Documents/config
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall 2&> /dev/null
 echo ". ~/Documents/config/bashrc" >> ~/.bashrc
 
 ln -sf ~/Documents/config/lilyterm.conf ~/.config/lilyterm/default.conf
 ln -sf ~/Documents/config/vimrc ~/.vimrc
 ln -sf ~/Documents/config/i3config ~/.i3/config
+
+vim +PluginInstall +qall
