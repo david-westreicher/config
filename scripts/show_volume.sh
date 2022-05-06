3cacc08d34eecb0f8f48825febfc903ee1c30ce7
david <david.westreicher@gmail.com>
2022-05-06 19:48:50 +0200

    Update media buttons, use default sink

--- scripts/show_volume.sh
+++ scripts/show_volume.sh
@@ -1,9 +1,9 @@
 function get_volume {
-    pacmd list-sinks | awk '/\tvolume:/ { print $5 }' | tail -n+$((sink_nr+1)) | head -n1 | cut -d '%' -f 1
+    pactl get-sink-volume @DEFAULT_SINK@ | awk '/Volume:/ { print $5 }' | cut -d '%' -f 1
 }
 
 volume=`get_volume`
-muted=`pacmd list-sinks | awk '/\tmuted:/ { print $2 }'`
+muted=`pactl get-sink-mute @DEFAULT_SINK@ | awk '/Mute:/ { print $2 }'`
 bar=$(seq -s "─" $(($volume / 5)) | sed 's/[0-9]//g')
 icon=$([[ "$muted" == "yes" ]] && echo "audio-volume-muted" || echo "audio-volume-high")
 dunstify -i $icon -r 502 -u low $bar
