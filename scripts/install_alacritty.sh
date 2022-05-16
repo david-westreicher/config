bf354dc40778190845210ff3c3a754733dc83497
David Westreicher <david.westreicher@gmail.com>
2022-05-16 20:30:36 +0200

    Fix building alacritty

--- scripts/install_alacritty.sh
+++ scripts/install_alacritty.sh
@@ -1,5 +1,5 @@
 cd /tmp
 git clone https://github.com/barnumbirr/alacritty-debian
 cd /tmp/alacritty-debian
-./build.sh
+./build.sh -i "debian:testing-slim"
 dpkg -i /tmp/alacritty-debian/target/alacritty_0.10.1-1_amd64.deb
