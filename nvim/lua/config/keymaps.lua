78abbd58cd91f55b2bc50e3a84ff56cb2fa62f1b
David Westreicher <david.westreicher@gmail.com>
2024-02-20 23:18:51 +0100

    nvim: cleanup, add documentation, check for new libs every week

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,7 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Fast ctrl-w/q actions
 vim.keymap.set("n", "<C-q>", vim.cmd.quitall)
+-- Fix neo-tree issue:
+-- https://github.com/nvim-neo-tree/neo-tree.nvim/wiki/FAQ#bdelete-makes-the-tree-spans-the-whole-window-how-do-i-prevent-it
 vim.keymap.set("n", "<C-w>", function()
   local find_buffer_by_type = function(type)
     for _, buf in ipairs(vim.api.nvim_list_bufs()) do
@@ -12,6 +16,7 @@ vim.keymap.set("n", "<C-w>", function()
     end
     return -1
   end
+
   local is_neotree_open = find_buffer_by_type("neo-tree") > 0
   if is_neotree_open then
     require("neo-tree.command").execute({ action = "close" })
@@ -20,11 +25,12 @@ vim.keymap.set("n", "<C-w>", function()
   else
     vim.cmd.bdelete()
   end
-end)
+end, { desc = "Close buffer/window" })
 
+-- Use default bindings for Shift-h/Shift-l
 vim.keymap.del("n", "<S-h>")
 vim.keymap.del("n", "<S-l>")
-vim.keymap.set("n", "<S-j>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
--- vim.keymap.set("n", "<S-k>", "<cmd>bnext<cr>", { desc = "Next buffer" })
---
-vim.keymap.set("n", "<S-Tab>", "<cmd>e #<cr>", { desc = "Switch to other buffer" })
+
+-- Buffer switching
+vim.keymap.set("n", "<S-j>", vim.cmd.bprevious, { desc = "Cycle buffer" })
+vim.keymap.set("n", "<S-Tab>", "<cmd>e #<cr>", { desc = "Switch to last buffer" })
