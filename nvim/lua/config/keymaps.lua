191f0d2dbcd6e947473b4282494d2ac81b93da84
David Westreicher <david.westreicher@gmail.com>
2024-02-20 22:46:31 +0100

    Fix neovim ctrl-w/q

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,9 +1,26 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
-vim.keymap.set("n", "<C-q>", vim.cmd.quit)
-vim.keymap.set("n", "<C-w>", vim.cmd.bdelete)
+vim.keymap.set("n", "<C-q>", vim.cmd.quitall)
+vim.keymap.set("n", "<C-w>", function()
+  local find_buffer_by_type = function(type)
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+      local ft = vim.api.nvim_buf_get_option(buf, "filetype")
+      if ft == type then
+        return buf
+      end
+    end
+    return -1
+  end
+  local is_neotree_open = find_buffer_by_type("neo-tree") > 0
+  if is_neotree_open then
+    require("neo-tree.command").execute({ action = "close" })
+    vim.cmd.bdelete()
+    require("neo-tree.command").execute({ action = "show" })
+  else
+    vim.cmd.bdelete()
+  end
+end)
 
 vim.keymap.del("n", "<S-h>")
 vim.keymap.del("n", "<S-l>")
